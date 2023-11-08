fac055c0839e88156b071d555f4e333a02dc3b61
Jovica Božić <112812542+JovicaB@users.noreply.github.com>
2023-11-08 14:09:32 +0100

    Create entrance.py

---
+++ entrance.py
@@ -0,0 +1,11 @@
+import datetime
+import json
+
+from datetime import datetime
+from data.data import PI5_DATA
+from data.database import DatabaseManager
+from data.json_data_manager import JSONData
+
+MEMBERSHIP_DATA = 'data/memebrship_data.json'
+ID_CARD_DATA = 'data/gym_id_card.json'
+LOCKERS_DATA = 'data/lockers.json'
